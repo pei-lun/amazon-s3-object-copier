9546c91c7b1eadc43a3de3692992c04831f6b70f
Pei-lun <Peilun0912@gmail.com>
2017-08-16 09:08:19 +0800

    Variables renamed

--- lambda_function.py
+++ lambda_function.py
@@ -12,17 +12,17 @@ def lambda_handler(event, context):
     record = sns_msg['Records'][0]
     source_bucket = str(record['s3']['bucket']['name'])
     source_key = str(unquote_plus(record['s3']['object']['key']))
-    target_bucket = environ['TARGET_BUCKET']
-    prefix = environ.get('PREFIX', '')
+    new_bucket = environ['BUCKET']
+    new_prefix = environ.get('PREFIX', '')
     if not source_key.endswith('/'):
         key = source_key.split('/')[-1]
         print(
             f'Copying s3://{source_bucket}/{source_key}'
-            f' to s3://{target_bucket}/{prefix}{key}.'
+            f' to s3://{new_bucket}/{new_prefix}{key}.'
         )
         s3.copy_object(
-            Bucket=target_bucket,
-            Key=f'{prefix}{key}',
+            Bucket=new_bucket,
+            Key=f'{new_prefix}{key}',
             CopySource={
                 'Bucket': source_bucket,
                 'Key': source_key
