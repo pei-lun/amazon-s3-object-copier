877c497476b7a578ad8a34f8d3154f8e39015376
Pei-lun <Peilun0912@gmail.com>
2017-10-16 17:16:29 +0800

    test_lambda_function.py updated

--- test_lambda_function.py
+++ test_lambda_function.py
@@ -4,22 +4,23 @@ from lambda_function import InvalidPrefix, generate_new_key
 
 
 def test_generate_new_key():
-    old_key = 'foo/bar/object'
-    skip = 'foo/'
-    new_prefix = 'hello/world/'
+    old_key = 'bar/baz/object'
+    skip = 'bar/'
+    new_prefix = 'foo/'
+    invalid_prefix = 'foo'
     with pytest.raises(InvalidPrefix):
-        generate_new_key(old_key, prefix='hello')
+        generate_new_key(old_key, prefix=invalid_prefix)
     with pytest.raises(InvalidPrefix):
-        generate_new_key(old_key, recursive=True, skip='foo')
+        generate_new_key(old_key, recursive=True, skip=invalid_prefix)
     assert generate_new_key(old_key) == 'object'
     assert generate_new_key(
-        old_key, prefix=new_prefix) == 'hello/world/object'
+        old_key, prefix=new_prefix) == 'foo/object'
     assert generate_new_key(old_key, prefix=new_prefix,
-                            recursive=True) == 'hello/world/foo/bar/object'
+                            recursive=True) == 'foo/bar/baz/object'
     # skip argument only works when recursive argument set to True.
     assert generate_new_key(old_key, skip=skip) == 'object'
     assert generate_new_key(old_key, prefix=new_prefix,
-                            skip=skip) == 'hello/world/object'
-    assert generate_new_key(old_key, recursive=True, skip=skip) == 'bar/object'
+                            skip=skip) == 'foo/object'
+    assert generate_new_key(old_key, recursive=True, skip=skip) == 'baz/object'
     assert generate_new_key(old_key, prefix=new_prefix, recursive=True,
-                            skip=skip) == 'hello/world/bar/object'
+                            skip=skip) == 'foo/baz/object'
