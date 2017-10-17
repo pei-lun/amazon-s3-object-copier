7272c55feeef4b66bd364de5595824815a673e46
Pei-lun <Peilun0912@gmail.com>
2017-10-17 10:47:33 +0800

    test_lambda_handler are added to test_lambda_function.py

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ An AWS Lambda function copying S3 object to another S3 bucket when receiving obj
 ## Usage
 
 After `s3://source/bar/baz/object` created, it will be copied to `s3://destination/foo/object` with `BUCKET` set to `destination` and `PREFIX` set to `foo/`. If `RECURSIVE` is set to `1`, the object will be copied to `s3://destination/foo/bar/baz/object`. You can set `SKIP` to `bar/` and the object will be copied to `s3://destination/foo/baz/object`.
+
+## Test
+
+Run `pip install boto3 moto pytest pytest-env` and then run `pytest`.
--- test_lambda_function.py
+++ test_lambda_function.py
@@ -1,6 +1,8 @@
+import boto3
 import pytest
+from moto import mock_s3
 
-from lambda_function import InvalidPrefix, generate_new_key
+from lambda_function import InvalidPrefix, generate_new_key, lambda_handler
 
 
 def test_generate_new_key():
@@ -24,3 +26,47 @@ def test_generate_new_key():
     assert generate_new_key(old_key, recursive=True, skip=skip) == 'baz/object'
     assert generate_new_key(old_key, prefix=new_prefix, recursive=True,
                             skip=skip) == 'foo/baz/object'
+
+
+@mock_s3
+def test_lambda_handler():
+    source_bucket = 'source'
+    source_key = 'bar/baz/object'
+    object_content = b'Hello World'
+    destination_bucket = 'destination'
+    destination_key = 'foo/baz/object'
+    sns_msg = (
+        f'{{"Records": [{{"s3": {{"bucket": {{"name": "{source_bucket}"}},'
+        f' "object": {{"key": "{source_key}"}}}}}}]}}'
+    )
+    event = {
+        'Records': [
+            {
+                'EventSource': 'aws:sns',
+                'EventVersion': '1.0',
+                'EventSubscriptionArn': 'event_subscription_arn',
+                'Sns': {
+                    'Type': 'Notification',
+                    'MessageId': 'message_id',
+                    'TopicArn': 'topic_arn',
+                    'Subject': 'Amazon S3 Notification',
+                    'Message': sns_msg,
+                    'Timestamp': '2017-10-17T01:35:48.774Z',
+                    'SignatureVersion': '1',
+                    'Signature': 'signature',
+                    'SigningCertUrl': 'signing_cert_url',
+                    'UnsubscribeUrl': 'unsubscribe_url',
+                    'MessageAttributes': {}
+                }
+            }
+        ]
+    }
+    s3_resource = boto3.resource('s3', region_name='us-east-1')
+    s3_resource.create_bucket(Bucket=source_bucket)
+    s3_resource.create_bucket(Bucket=destination_bucket)
+    s3_client = boto3.client('s3')
+    s3_client.put_object(Bucket=source_bucket, Key=source_key,
+                         Body=object_content)
+    lambda_handler(event)
+    copied_object = s3_resource.Object(destination_bucket, destination_key)
+    assert object_content == copied_object.get()['Body'].read()
