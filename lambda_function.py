cfeea0f4ec59eac98bc1284955dd8c8a956bd89e
Pei-Lun Huang <peilun0912@gmail.com>
2020-06-11 21:18:25 +0800

    feat: lambda_function.py can be invoked by S3 event

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,4 @@
-import ast
+import json
 from os import environ
 from urllib.parse import unquote_plus
 
@@ -6,33 +6,41 @@ import boto3
 
 
 def lambda_handler(event, *_):
-    s3 = boto3.client('s3')
-    sns_msg = ast.literal_eval(event['Records'][0]['Sns']['Message'])
-    record = sns_msg['Records'][0]
-    source_bucket = str(record['s3']['bucket']['name'])
-    source_key = str(unquote_plus(record['s3']['object']['key']))
     new_bucket = environ['BUCKET']
     new_prefix = environ.get('PREFIX', '')
     recursive = environ.get('RECURSIVE', '0')
     skip = environ.get('SKIP', '')
+
+    record: dict = event['Records'][0]
+    if record.get('EventSource') == 'aws:sns':
+        s3_event = json.loads(record['Sns']['Message'])
+        source_bucket = s3_event['Records'][0]['s3']['bucket']['name']
+        source_key = unquote_plus(
+            s3_event['Records'][0]['s3']['object']['key']
+        )
+    elif record.get('eventSource') == 'aws:s3':
+        source_bucket = record['s3']['bucket']['name']
+        source_key = unquote_plus(record['s3']['object']['key'])
+    else:
+        print(f'Unrecognized event: "{event}".')
+        return
+
+    s3_client = boto3.client('s3')
     if not source_key.endswith('/'):
         new_key = generate_new_key(
             source_key,
             prefix=new_prefix,
             recursive=bool(int(recursive)),
-            skip=skip
+            skip=skip,
         )
         print(
             f'Copying object from s3://{source_bucket}/{source_key}'
             f' to s3://{new_bucket}/{new_key}.'
         )
-        s3.copy_object(
+        s3_client.copy_object(
             Bucket=new_bucket,
             Key=new_key,
-            CopySource={
-                'Bucket': source_bucket,
-                'Key': source_key
-            }
+            CopySource={'Bucket': source_bucket, 'Key': source_key},
         )
 
 
